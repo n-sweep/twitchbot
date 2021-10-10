0fdaa38182bec224768bda5f6955d59e588ae945
Noah Shreve <34486798+n-sweep@users.noreply.github.com>
2021-10-10 18:27:03 -0400

    remove endopoint from run.py

--- run.py
+++ run.py
@@ -2,19 +2,14 @@
 # A Twitch Bot
 
 import os
-import asyncio
 from bot.core import Bot
-from auth.endpoint import Endpoint
 
 d = os.path.dirname(os.path.realpath(__file__))
 fp = os.path.join(d, 'conf/config.json')
 
 
 def main():
-    loop = asyncio.get_event_loop()
-    ept = Endpoint(fp)
     bot = Bot(fp)
-    loop.create_task(ept.run())
     bot.run()
 
 
